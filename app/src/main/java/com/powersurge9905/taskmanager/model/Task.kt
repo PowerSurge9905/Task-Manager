5c93090f20ddb441bf203b4033e078131b554b51
Dominic Anderson <powersurge9905@gmail.com>
2026-02-19 09:41:43 -0500

    Version 0.1.1
    Adjusted Task.kt (no longer a data class)

--- app/src/main/java/com/powersurge9905/taskmanager/model/Task.kt
+++ app/src/main/java/com/powersurge9905/taskmanager/model/Task.kt
@@ -1,9 +1,5 @@
 package com.powersurge9905.taskmanager.model
 
-import androidx.annotation.BoolRes
-import androidx.annotation.StringRes
-
-data class Task(
-    @StringRes val stringResourceId: Int,
-    @BoolRes val boolResourceId: Int
-)
+class Task(val name: String) {
+    var complete: Boolean = false
+}
